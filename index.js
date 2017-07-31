d2d95d7c2c9c0f55a283c75833b8e7d74305c286
Michael Angerman <stormasm@gmail.com>
2017-07-31 16:55:08 -0700

    package.json

--- index.js
+++ index.js
@@ -3,59 +3,10 @@ import 'babel-core/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import ItemThreadList from './containers/ItemThreadList';
-import TableThreadList from './containers/TableThreadList';
+import CRUDTable from './src/components/crud-redux';
 import configureStore from './store/configureStore';
 
-/* Table Imports */
-
-import TableHn from "./components/TableHn";
-import TableFriends from "./components/TableFriends";
-import TableFamily from "./components/TableFamily";
-import TableCrud from "./components/TableCrud";
-import { Flex, Box } from "grid-styled";
-
-/* Tab Imports */
-
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
-import './public/stylesheets/react-tabs.css';
-
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
-  <Tabs>
-    <TabList>
-      <Tab>TableHn</Tab>
-      <Tab>MyHn</Tab>
-      <Tab>TableBasic</Tab>
-      <Tab>TableCrud</Tab>
-      <Tab>Hn</Tab>
-    </TabList>
-  <TabPanel>
-    <TableHn />
-  </TabPanel>
-  <TabPanel>
-    <TableThreadList />
-  </TabPanel>
-  <TabPanel>
-  <Flex>
-    <Box width={1 / 2} px={2}>
-      <TableFriends />
-    </Box>
-    <Box width={1 / 2} px={2}>
-      <TableFamily />
-    </Box>
-  </Flex>
-  </TabPanel>
-  <TabPanel>
-    <TableCrud />
-  </TabPanel>
-  <TabPanel>
-    <ItemThreadList />
-  </TabPanel>
-  </Tabs>
-  </Provider>,
+  <CRUDTable />
   document.getElementById('root')
 );
