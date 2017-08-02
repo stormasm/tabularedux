6681cd534a202b3e58b440360bb089fb9ef8da62
Michael Angerman <stormasm@gmail.com>
2017-08-02 13:32:03 -0700

    attempting to break out the reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,59 @@
+import { cloneDeep, findIndex } from 'lodash';
+import { combineReducers } from 'redux'
+
+const editProperty = (rows, index, values) => {
+  // Skip mutation, there's likely a neater way to achieve this
+  const ret = cloneDeep(rows);
+
+  Object.keys(values).forEach(v => {
+    ret[index][v] = values[v];
+  });
+
+  return ret;
+}
+
+const rowReducer = (state, action) => {
+  const row = action.row;
+  const index = row && findIndex(state, { id: row.id });
+
+  switch (action.type) {
+    case 'CREATE_ROW':
+      return [row].concat(state);
+
+    case 'DELETE_ROW':
+      if (index >= 0) {
+        return state.slice(0, index).concat(state.slice(index + 1));
+      }
+
+    case 'EDIT_ROW':
+      if (index >= 0) {
+        return editProperty(state, index, {
+          editing: row.columnIndex
+        });
+      }
+
+    case 'CONFIRM_EDIT':
+      if (index >= 0) {
+        return editProperty(state, index, {
+          [row.property]: row.value,
+          editing: false
+        });
+      }
+
+    case 'SAVE_ROW':
+    if (index >= 0) {
+        console.log(index,state[index])
+    }
+
+    default:
+      return state;
+  }
+
+  return state;
+};
+
+const rootReducer = combineReducers({
+  rowReducer
+})
+
+export default rootReducer
