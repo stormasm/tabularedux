6af4741545d4b656f24b0c4b8e11037baf8cfd41
Michael Angerman <stormasm@gmail.com>
2017-08-02 18:10:04 -0700

    static json objects up and running

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -28,9 +28,28 @@ const schema = {
   required: ['id', 'threadid', 'name', 'position', 'active']
 };
 
+const my2genrows = () =>
+    [
+    {
+        "active":false,
+        "by":"Homer Johnson",
+        "id":"dcf6cd91-8c29-4430-ae79-369f5b610567",
+        "score": 6212,
+        "threadid": 3987,
+        "title": "Boss"
+    }
+    ]
+
+const mygenrows = () => {
+    const myrows = generateRows(3,schema);
+    console.log(myrows);
+    return myrows;
+}
+
 const configureStore = () => createStore(
     rowReducer,
-    generateRows(3,schema),
+    //generateRows(3,schema),
+    my2genrows(),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
